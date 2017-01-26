7921e560f5bcd91ec8fdc526b8aaa444e5ab4bde
Sajedah.93 <your_email@whatever.comcclear>
2017-01-26 09:06:40 +0200

    SOLVED

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,7 +10,40 @@
  * console.log(index); // 4
  */
 
-var binarySearch = function (array, target) {
+// var binarySearch = function (array, target) {
+// 	// var x = 0 ; 
+// 	// var llength = array.length-1 ;
+// 	for (var i = 0; i < array.length; i++) {
+// 		if(array[i] === target){
+// 			return i
+// 		}
+// 	}
+
   
-};
+// };
+
+
+
+var binarySearch = function (array, target) {
+	var first = 0 ; 
+	var last = array.length-1 ; 
+	var mid = Math.floor((last+first)/2)
+	while( first < last){
+		if(array[mid] !== target){
+			if (target < array[mid]){
+				last = mid - 1;
+			} else if (target > array[mid]){
+				first = mid + 1;
+			}
+			mid = Math.floor((last + first)/2);
+        // console.log(mid)
+    }
+    else{
+    	return mid;
+    }
+}
+}
+
+
+
 
